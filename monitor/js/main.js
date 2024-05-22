3ef227e22fad7a9cc8859a0b6c4600c76d8416bb
Mikhael <138624619+Mikhael127@users.noreply.github.com>
2024-05-21 23:47:03 -0600

    Add files via upload

---
+++ monitor/js/main.js
@@ -0,0 +1,36 @@
+let respuesta = document.getElementById("respuesta");
+let respuestaServidor;
+
+function callApiRequest(estatus) {
+  // Hacer una petición para un usuario con ID especifico
+  axios
+    .get("http://127.0.0.1/iot-car-control/back-end/apis/getRegistro.php")
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response);
+
+      respuestaServidor =
+        respuesta.data == "W"
+          ? "Adelante": 
+          response.data == "S"
+          ? "Retroceder":
+          response.data == "A"
+          ? "Izquierda":
+          response.data == "D"
+          ? "Derecha": 
+          response.data == "X"
+          ? "Detenerse": "";
+
+      respuesta.innerHTML =
+        "<strong> Respuesta: " + respuestaServidor + "</strong>";
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // siempre será ejecutado
+    });
+}
+
+setInterval(callApiRequest, 2000);
